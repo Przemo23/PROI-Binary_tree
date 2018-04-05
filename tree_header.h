891147d3833fde8f0ac2e9d836f00b01dad3e62c
Przemo23 <37190214+Przemo23@users.noreply.github.com>
2018-04-05 12:50:33 +0200

    Update tree_header.h

--- tree_header.h
+++ tree_header.h
@@ -5,12 +5,27 @@
 class tree
 {
     public:
-
+    //constructor
     tree(int);
+    //destructor
     ~tree() { destroy_tree(root);}
 
+    //adding and removing nodes
     void add_node(int);
     node* remove_node(node*,int);
+
+    //methods interacting with other trees
+    void add_trees(node*);
+    void substract_trees(node*);
+    bool compare_trees(tree*);
+
+    int counter;
+    node *root;
+
+    void print_tree(node*);
+
+    private:
+    //methods that are parts of other methods within other methods within other methods etc.
     int get_lenght(node*);
     bool check_if_member(int);
     node* find_parent_node(int,node*);
@@ -18,11 +33,30 @@ class tree
     node* get_node(int);
     int get_value(int);
     void destroy_tree(node*);
-     node* min_value_node( node*);
+    node* min_value_node( node*);
+    bool compare_nodes(node*,node*);
 
 
-        int counter;
-        node *root;
+
+
+
+    //operators, not working yet
+    tree* operator+=(const tree& temp)
+    {add_trees(temp.root);
+        return this;}
+    tree* operator-=(const tree& temp)
+     {substract_trees(temp.root);
+     return this;}
+     bool operator==( tree*& temp)
+     {
+         return compare_trees(temp);
+
+     }
+
+
+
+
+
 
 
 
